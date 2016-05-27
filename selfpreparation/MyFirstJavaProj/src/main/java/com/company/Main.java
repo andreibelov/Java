de4187cbd4c50bf34b6ad2e7480377bdc52a9db9
andreibelov <andrei.belov@mail.ru>
2016-05-27 21:26:06 +0300

    ^_^

---
+++ selfpreparation/MyFirstJavaProj/src/main/java/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+
+        public class Main {
+
+            public static void main(String[] args) {
+                // Prints "^_^" to the terminal window.
+                System.out.println("^_^");
+            }
+
+        }
+
+
